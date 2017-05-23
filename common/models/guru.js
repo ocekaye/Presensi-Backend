81e09111a6b7e00a6d6d308991a37c9bdb20b8e5
ocekaye <styat_moko@rocketmail.com>
2017-05-23 23:35:03 +0700

    add all tugas detail

--- common/models/guru.js
+++ common/models/guru.js
@@ -16,6 +16,23 @@ module.exports = function(Guru) {
         cb(null, jadwal);
       }
     });
+  };
 
+  Guru.remoteMethod('tugasdetail', {
+    http: { path: '/:id/tugas/detail', verb: 'get' },
+    accepts: [
+      { arg: 'id', type: 'string', required: true, description: 'Guru id.'}
+    ],
+    returns: {arg: 'result', type: 'string', root: true}
+  });
+
+  Guru.tugasdetail = function(id, cb) {
+
+    Guru.prototype.tugas({where: {guruId: id}, include: ['kelas', 'mapel']}, function (err, tugas) {
+      if (err) cb(err);
+      else {
+        cb(null, tugas);
+      }
+    });
   }
 };
